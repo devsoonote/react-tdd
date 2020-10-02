32ff529abbf57ea42bf594369a71d9bf2425535e
devsoonote <sookung93@gmail.com>
2020-10-02 22:59:26 +0900

    feat: Manual Acceptance Testing
    - change app component to class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
+import GuessedWords from "./GuessedWords";
+import Congrats from "./Congrats";
 
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <h1>Jotto</h1>
+        <Congrats success={true} />
+        <GuessedWords guessedWords={[{
+          guessedWord: 'train',
+          letterMatchCount: 3
+        }]} />
+      </div>
+    )
+  }
 }
 
 export default App;
