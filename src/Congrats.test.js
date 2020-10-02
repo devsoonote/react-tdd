9c164c3375f35e4f4dfc4fdbc25cc33f28d18dfe
devsoonote <sookung93@gmail.com>
2020-10-02 19:49:59 +0900

    feat: Congrats Component Test Setup
    - create findByTestAttr function
    - add render test code

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,1 +1,23 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
 
+import { findByTestAttr } from '../test/testUtils';
+import Congrats from "./Congrats";
+
+/*
+* Factory function to create a ShallowWrapper for the Congrats component.
+* @Function setup
+* @param {object} props - Component props specific to this setup.
+* @returns {ShallowWrapper}
+* */
+const setup = (props = {}) => shallow(<Congrats {...props} />);
+
+test('render without error', () => {
+
+})
+test('render no text when `success` prop is false', () => {
+
+})
+test('renders non-empty congrats message when `success` prop is true', () => {
+
+})
