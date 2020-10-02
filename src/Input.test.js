37aa460c4cad9bb0a0bc04968ab17034f898ba51
devsoonote <sookung93@gmail.com>
2020-10-03 03:27:06 +0900

    test: Input Tests and Code for success=false

---
+++ src/Input.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { findByTestAttr, storyFactory } from "../test/testUtils";
+import Input from "./Input";
+
+const setup = (initialState = {}) => {
+  const store = storyFactory(initialState)
+  return shallow(<Input store={store}/>).dive().dive();
+}
+
+describe('render', () => {
+  describe('word has not been guessed', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = setup({ success: false })
+    })
+
+    test('renders component without error', () => {
+      const component = findByTestAttr(wrapper, 'component-input')
+      expect(component.length).toBe(1)
+    })
+    test('renders input box', () => {
+      const inputBox = findByTestAttr(wrapper, 'input-box')
+      expect(inputBox.length).toBe(1)
+    })
+    test('renders submit button', () => {
+      const submitButton = findByTestAttr(wrapper, 'submit-button')
+      expect(submitButton.length).toBe(1)
+    })
+  })
+  describe('word has been guessed', () => {
+    test('renders component without error', () => {})
+    test('does not renders input box', () => {})
+    test('does not renders submit button', () => {})
+  })
+})
+
+describe('update state', () => {
+
+})
