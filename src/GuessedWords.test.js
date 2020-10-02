629870b3d1cf7293e45cb902b2cea4b91970db56
devsoonote <sookung93@gmail.com>
2020-10-02 21:39:27 +0900

    test: Use 'describe' for Context

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -22,3 +22,11 @@ const setup = (props = {}) => {
 test('does not throw waring with expected props', () => {
   checkProps(GuessedWords, defaultProps)
 })
+
+describe('if there are no words guessed', () => {
+
+})
+
+describe('if there are words guessed', () => {
+
+})
