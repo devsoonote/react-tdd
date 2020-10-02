a9709442842192de051d0f8ad2854183872d5621
devsoonote <sookung93@gmail.com>
2020-10-03 03:06:15 +0900

    test: Enzyme `dive()` method

--- src/actions/Input.test.js
+++ src/actions/Input.test.js
@@ -6,11 +6,9 @@ import Input from "../Input";
 
 const setup = (initialState = {}) => {
   const store = storyFactory(initialState)
-  const wrapper = shallow(<Input store={store}/>)
-  console.log(wrapper.debug())
+  const wrapper = shallow(<Input store={store}/>).dive().dive()
+  return wrapper;
 }
-
-setup()
 
 describe('render', () => {
   describe('word has not been guessed', () => {
