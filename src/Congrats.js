e4f68b0dd99fbc093d019bfc6c6ec462e076ae99
devsoonote <sookung93@gmail.com>
2020-10-02 19:39:33 +0900

    feat: Congrats Component Test Setup
    - install enzyme-adapter-react-16, enzyme, jest-enzyme
    - initial setup in setupTest
    - create Congrats Component

---
+++ src/Congrats.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+/*
+* Functional react component for congratulatory message.
+* @function
+* @return {JSX.Element} - Rendered component (or null if 'success' prop is false)
+* */
+
+export default () => {
+  return (
+    <div></div>
+  )
+}
