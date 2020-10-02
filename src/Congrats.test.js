884efc307838fdcb4118f11d0e9af9b7a500ed0e
devsoonote <sookung93@gmail.com>
2020-10-02 20:12:56 +0900

    test: PropTypes Testing
    - install prop-types and check-prop-types
    - create checkProps function in testUtils
    - add does not throw warning with expected props test

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 
-import { findByTestAttr } from '../test/testUtils';
+import { findByTestAttr, checkProps } from '../test/testUtils';
 import Congrats from "./Congrats";
 
 /*
@@ -26,4 +26,8 @@ test('renders non-empty congrats message when `success` prop is true', () => {
   const wrapper = setup({ success: true });
   const message = findByTestAttr(wrapper, 'congrats-message')
   expect(message.text().length).not.toBe(0);
+})
+test('does not throw warning with expected props', () => {
+  const expectedProps = { success: false }
+  checkProps(Congrats, expectedProps);
 })
--- test/testUtils.js
+++ test/testUtils.js
@@ -1,3 +1,5 @@
+import checkPropTypes from 'check-prop-types';
+
 
 /*
 * Return node(s) with the given data-test attribute.
@@ -7,3 +9,13 @@
 * */
 
 export const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`)
+
+export const checkProps = (component, conformingProps) => {
+  const propError = checkPropTypes(
+    component.propTypes,
+    conformingProps,
+    'prop',
+    component.name
+    )
+  expect(propError).toBeUndefined();
+}
