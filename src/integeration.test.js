6ae50420ccc4b53060fffb6bde9144aa257bed9e
devsoonote <sookung93@gmail.com>
2020-10-04 00:44:34 +0900

    test: First Thunk Test and Add 'secretWordReducer'

--- src/integeration.test.js
+++ src/integeration.test.js
@@ -2,9 +2,26 @@ import { storyFactory } from '../test/testUtils'
 import { guessWord } from './actions'
 
 describe('guessWord action dispatcher', () => {
+  const secretWord = 'party'
+  const unsuccessfulGuess = 'train'
   describe('no guessed words', () => {
+    let store
+    const initialState = { secretWord }
+    beforeEach(() => {
+      store = storyFactory(initialState)
+    })
     test('updates state correctly for unsuccessful guess', () => {
-
+      store.dispatch(guessWord(unsuccessfulGuess))
+      const expectedState = {
+        ...initialState,
+        success: false,
+        guessWord: [{
+          guessedWord: unsuccessfulGuess,
+          letterMatchCount: 3
+        }]
+      }
+      const newState = store.getState()
+      expect(newState).toEqual(expectedState)
     })
     test('updates state correctly for successful guess', () => {
 
