cccb1f2549fabc939acd8fabecd29610ae853b63
devsoonote <sookung93@gmail.com>
2020-10-04 00:53:54 +0900

    test: Remaining Test

--- src/integeration.test.js
+++ src/integeration.test.js
@@ -12,6 +12,7 @@ describe('guessWord action dispatcher', () => {
     })
     test('updates state correctly for unsuccessful guess', () => {
       store.dispatch(guessWord(unsuccessfulGuess))
+      const newState = store.getState()
       const expectedState = {
         ...initialState,
         success: false,
@@ -20,19 +21,51 @@ describe('guessWord action dispatcher', () => {
           letterMatchCount: 3
         }]
       }
-      const newState = store.getState()
       expect(newState).toEqual(expectedState)
     })
     test('updates state correctly for successful guess', () => {
-
+      store.dispatch(guessWord(secretWord))
+      const newState = store.getState()
+      const expectedState = {
+        ...initialState,
+        success: true,
+        guessWord: [{
+          guessedWord: secretWord,
+          letterMatchCount: 5
+        }]
+      }
+      expect(newState).toEqual(expectedState)
     })
   })
   describe('some guessed words', () => {
+    const guessedWords = [ { guessedWord: 'agile', letterMatchCount: 1 }]
+    const initialState = { guessedWords, secretWord }
+    let store;
+    beforeEach(() => {
+      store = storyFactory(initialState)
+    })
     test('updates state correctly for unsuccessful guess', () => {
-
+      store.dispatch(guessWord(unsuccessfulGuess))
+      const newState = store.getState()
+      const expectedState = {
+        secretWord,
+        success: false,
+        guessedWords: [...guessedWords, { guessedWord: unsuccessfulGuess, letterMatchCount: 3 }]
+      }
+      expect(newState).toEqual(expectedState)
     })
     test('updates state correctly for successful guess', () => {
-
+      store.dispatch(guessWord(secretWord))
+      const newState = store.getState()
+      const expectedState = {
+        secretWord,
+        success: true,
+        guessWord: [
+          ...guessedWords,
+          { guessedWord: secretWord, letterMatchCount: 5 }
+        ]
+      }
+      expect(newState).toEqual(expectedState)
     })
   })
 })
